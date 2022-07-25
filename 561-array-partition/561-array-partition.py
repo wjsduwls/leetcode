1c732ed813cacf0d3fb2363da1949c71b5661b80
wjsduwls <64457259+wjsduwls@users.noreply.github.com>
2022-07-26 01:37:34 +0900

    Time: 576 ms (9.05%), Space: 16.7 MB (78.40%) - LeetHub

--- 561-array-partition/561-array-partition.py
+++ 561-array-partition/561-array-partition.py
@@ -3,6 +3,5 @@ class Solution:
         nums.sort()
         answer = 0
         for i in range(0, len(nums), 2):
-            print(nums[i:i+2])
             answer += min(nums[i:i+2])
         return answer
